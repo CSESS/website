a6537432eaab8d06f0fa740da77b245914175175
JYWTom <74043061+JYWTom@users.noreply.github.com>
2025-05-13 22:32:08 +0800

    temporary disable service worker

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -12,4 +12,4 @@ ReactDOM.createRoot(root).render(
   </BrowserRouter>
 );
 
-registerServiceWorker();
+// registerServiceWorker();
