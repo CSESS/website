8004ad45fca01ae617e6ace98832fce4444bde26
JYWTom <74043061+JYWTom@users.noreply.github.com>
2025-05-13 22:50:34 +0800

    enable service worker

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
 import "./index.css";
-// import registerServiceWorker from "./registerServiceWorker";
+import registerServiceWorker from "./registerServiceWorker";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
 );
 
-// registerServiceWorker();
+registerServiceWorker();
