294a5de89aa9a38aacccaf11ec11be84b6ed511a
JYWTom <74043061+JYWTom@users.noreply.github.com>
2025-05-18 23:24:03 +0800

    better polyfill

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from '@vitejs/plugin-legacy';
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(() => {
   return {
     build: {
       outDir: "build",
-      target: ["es6"],
     },
     base: "/",
     plugins: [
+      legacy({
+        modernPolyfills: ["es.object.group-by"],
+        renderLegacyChunks: false
+      }),
       react(),
       VitePWA({
         workbox: {
